2b62327a7d707c697c560c3da59df948fb7650fb
Khaydir Murteza <khaydirmurteza@gmail.com>
2026-03-30 05:19:15 +0700

    menambahkan kelas DataGeneric dengan method PrintData

--- TP_MODUL5_103022430017/Program.cs
+++ TP_MODUL5_103022430017/Program.cs
@@ -1,1 +1,42 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class HaloGeneric
+{
+    public void SapaUser<X>(X user)
+    {
+        Console.WriteLine("Halo user: " + user);
+    }
+}
+
+class DataGeneric<T>
+{
+    public T data;
+
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void printData()
+    {
+        Console.WriteLine("Data yang tersimpan: " + data);
+    }
+}
+
+class Utama
+{
+    static void Main(String[] args)
+    {
+        HaloGeneric input = new HaloGeneric();
+
+        Console.WriteLine("Masukkan nama");
+        Console.WriteLine("Masukkan NIM: ");
+        String nama = Console.ReadLine();
+        String nim = Console.ReadLine();
+        input.SapaUser<string>(nama);
+
+        DataGeneric<String> data = new DataGeneric<String>(nim);
+
+        data.printData();
+    }
+}
